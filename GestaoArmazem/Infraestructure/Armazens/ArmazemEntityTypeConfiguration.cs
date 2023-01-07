7d09ed3cf3b50621a240523906a4bbbe1cc23f85
Luís Carlos <1190818@isep.ipp.pt>
2023-01-07 00:17:26 +0000

    Update ArmazemEntityTypeConfiguration.cs

--- GestaoArmazem/Infraestructure/Armazens/ArmazemEntityTypeConfiguration.cs
+++ GestaoArmazem/Infraestructure/Armazens/ArmazemEntityTypeConfiguration.cs
@@ -3,24 +3,33 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using DDDSample1.Domain.Armazens;
 
-namespace DDDSample1.Infrastructure.Armazens {
-    public class ArmazemEntityTypeConfiguration : IEntityTypeConfiguration<Armazem> {
-        public void Configure(EntityTypeBuilder<Armazem> entityTypeBuilder) {
+namespace DDDSample1.Infrastructure.Armazens
+{
+    public class ArmazemEntityTypeConfiguration : IEntityTypeConfiguration<Armazem>
+    {
+        public void Configure(EntityTypeBuilder<Armazem> entityTypeBuilder)
+        {
             entityTypeBuilder.HasKey(armazem => armazem.Id);
             entityTypeBuilder.OwnsOne(armazem => armazem.Designacao,
-                designação => {designação.Property("texto").IsRequired(true);});
-            entityTypeBuilder.OwnsOne(armazem => armazem.Endereco, endereco => { 
+                designação => { designação.Property("texto").IsRequired(true); });
+            entityTypeBuilder.OwnsOne(armazem => armazem.Endereco, endereco =>
+            {
                 endereco.Property("codigoPostal").IsRequired(true);
                 endereco.Property("numeroPorta").IsRequired(true);
                 endereco.Property("nomeRua").IsRequired(true);
                 endereco.Property("localidade").IsRequired(true);
                 endereco.Property("pais").IsRequired(true);
-                });
+            });
             entityTypeBuilder.OwnsOne(armazem => armazem.Municipio,
-                municipio => {municipio.Property("nome").IsRequired(true);});
-            entityTypeBuilder.OwnsOne(armazem => armazem.Coordenadas, coordenadas => {
+                municipio => { municipio.Property("nome").IsRequired(true); });
+            entityTypeBuilder.OwnsOne(armazem => armazem.Coordenadas, coordenadas =>
+            {
                 coordenadas.Property("latitude").IsRequired(true);
                 coordenadas.Property("longitude").IsRequired(true);
+            });
+            entityTypeBuilder.OwnsOne(armazem => armazem.Altitude, altitude =>
+            {
+                altitude.Property("altitude").IsRequired(true);
             });
         }
     }
